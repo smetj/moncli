1bf6e12b6a734f23f1a0d7d02b3efa1a6cb72efa
Jelle Smet <development@smetj.net>
2012-05-17 23:13:14 +0200

    changed wishbone locking

--- lib/modules/moncli/moncli.py
+++ lib/modules/moncli/moncli.py
@@ -90,7 +90,6 @@ class Scheduler(PrimitiveActor):
     
     def shutdown(self):
         '''When called during shutdown will trigger the save() function.'''
-        self.lock=False
         self.save()
         self.logging.info('Shutdown')
     
